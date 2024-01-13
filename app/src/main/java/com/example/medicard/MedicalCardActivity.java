1d6ee6a90cd751962aee6656855eb60d9b29b813
Sanket <sanketnaik20799@gmail.com>
2024-01-13 07:43:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/medicard/MedicalCardActivity.java
@@ -0,0 +1,14 @@
+package com.example.medicard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_card);
+    }
+}
